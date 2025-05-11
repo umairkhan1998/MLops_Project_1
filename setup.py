c30f10e5c4d0636e68f0ada6714be40ffe5ffaac
umairkhan1998 <umairkhan.jk1998@gmail.com>
2025-05-11 19:58:50 +0500

    The struture of the project is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Umair Abbas",
+    author_email="umairkhan.jk1998@gmail.com",
+    packages=find_packages()
+)
